a0e4565fbffd6fb8dab3a7d64fdba84a6c1aa3a8
RodrigoMato00 <3293@holbertonschool.com>
2022-01-03 10:48:33 -0800

    correccion 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,6 @@
 #create a file in tmp
 
-file { 'school':
-  ensure = 'present'
-  path    => '/tmp/school',
+file { 'tmp/school':
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
