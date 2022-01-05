4506c67f8e789f68968c8b2c77115e8d69627e20
RodrigoMato00 <3293@holbertonschool.com>
2022-01-04 17:35:33 -0800

    correccion 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 #make changes to our configuration file
 
 exec { 'echo':
-path    => '/bin/',
-command => 'echo "IdentityFile ~/.ssh/school\n PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  path    => '/bin/',
+  command => 'echo "IdentityFile ~/.ssh/school\n PasswordAuthentication no" >> /etc/ssh/ssh_config',
 }
