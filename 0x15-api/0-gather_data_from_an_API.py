6b1987fa7d4f2be14690940d009ff1d9bfd5a0f2
RodrigoMato00 <3293@holbertonschool.com>
2022-01-31 20:10:30 -0300

    0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
     url = "https://jsonplaceholder.typicode.com/users/"
     user = requests.get(url + "users/{}".format(argv[1])).json()
-    todo = requests.get(url + sys.argv[1] + "/todos").json()
+    todo = requests.get(url + "todos", params={"userId": argv[1]}).json()
 
     tasks = []
     for task in todo:
