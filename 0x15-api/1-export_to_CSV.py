e0ca61237901c142287f0a217440013aa47637b2
RodrigoMato00 <3293@holbertonschool.com>
2022-01-31 23:12:53 -0300

    1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-""" Export data in the CSV format.. """
-
+"""
+Using what you did in the task #0,
+extend your Python script to export data in the CSV format.
+Requirements:
+Records all tasks that are owned by this employee
+Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+File name must be: USER_ID.csv
+"""
 
 if __name__ == "__main__":
     import json
     import requests
     import sys
-    
+
     req = requests.get("https://jsonplaceholder.typicode.com/users/" +
                        sys.argv[1])
     dic = json.loads(req.text)
