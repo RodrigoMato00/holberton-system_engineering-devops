5341f54dd29208d7383259aac94d6b5378d5fe73
RodrigoMato00 <3293@holbertonschool.com>
2022-01-31 22:05:40 -0300

    v final

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,23 +5,22 @@ for a given employee ID,
 returns information about
 his/her TODO list progress
 """
+import json
 import requests
 from sys import argv
 
 if __name__ == '__main__':
 
-    if len(argv) == 2 and argv[1].isdigit():
-        url = 'https://jsonplaceholder.typicode.com/'
-        user = requests.get(url+'users/{}'.format(argv[1])).json()["name"]
-        request = requests.get(url+'todos?userId={}'.format(argv[1]))
+    req = requests.get("https://jsonplaceholder.typicode.com/users/" + argv[1])
+    dic = json.loads(req.text)
+    name = dic.get('name')
+    req1 = requests.get("https://jsonplaceholder.typicode.com/todos/" + "?userId=" + argv[1])
 
-        dic = request.json()
-        number = 0
-        t_name = ""
+    todos = json.loads(req1.text)
+    tasks = len(todos)
+    done = [task for task in todos if task.get('completed')]
+    tasks_done = len(done)
+    print("Employee {} is done with tasks({}/{}):".format(name, tasks_done, tasks))
 
-        for task in dic:
-            if task['completed'] is True:
-                number += 1
-                t_name += "\t " + task["title"] + '\n'
-        print("Employee {} is done with tasks({}/20):".format(user, number))
-        print(t_name, end="")
+    for task in done:
+        print("\t", task.get('title'))
