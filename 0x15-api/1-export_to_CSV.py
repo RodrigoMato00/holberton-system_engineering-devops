a80ebf06e36ba91b99f7425729bdeac405964c92
RodrigoMato00 <3293@holbertonschool.com>
2022-01-31 23:00:59 -0300

    1 funcional

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Export data in the CSV format.. """
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    req = requests.get("https://jsonplaceholder.typicode.com/users/" + sys.argv[1])
+    dic = json.loads(req.text)
+    username = dic.get('username')
+    req1 = requests.get("https://jsonplaceholder.typicode.com/todos/" + "?userId=" + sys.argv[1])
+
+    todos = json.loads(req1.text)
+    tasks = [task for task in todos]
+
+    with open(sys.argv[1] + ".csv", "w") as file:
+
+        for task in tasks:
+            data = ['"' + sys.argv[1] + '"', '"' +
+                    username + '"', '"' +
+                    str(task.get('completed')) + '"', '"' +
+                    task.get('title') + '"']
+
+            file.write(",".join(data) + '\n')
